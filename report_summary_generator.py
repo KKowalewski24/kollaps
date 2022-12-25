c97ef3d0b68df9ca49df1b165e8cf9bd6236457e
Kamil Kowalewski <szamil24@gmail.com>
2022-12-25 12:11:40 +0100

    Prepare writing data to file

--- report_summary_generator.py
+++ report_summary_generator.py
@@ -1,8 +1,9 @@
 import glob
 from argparse import ArgumentParser, Namespace
-from typing import List
+from typing import List, Tuple
 
 from docx import Document
+from docx.enum.text import WD_BREAK
 
 """
     How to run:
@@ -10,20 +11,36 @@ from docx import Document
 """
 
 DOCX_EXTENSION: str = ".docx"
+OUTPUT_FILENAME: str = "summary"
+DATE_LABEL: str = "Data:"
+MODE_LABEL: str = "Tryb:"
+SUMMARY_LABEL: str = "Podsumowanie:"
 
 
 def main() -> None:
     args = prepare_args()
     path: str = args.path
-    file_paths: List[str] = glob.glob(f"{path}*{DOCX_EXTENSION}")
+    filepaths: List[str] = glob.glob(f"{path}*{DOCX_EXTENSION}")
 
-    for filepath in file_paths:
-        print(filepath)
+    output_document = Document()
+    for filepath in filepaths:
+        input_document: Document = Document(filepath)
+        date, mode, summary = extract_information(input_document)
 
-    document: Document = Document(file_paths[0])
-    print([x.text for x in document.paragraphs if x.text == "Data:"])
-    for paragraph in document.paragraphs:
-        print(paragraph.text)
+        output_document.add_paragraph(f"{DATE_LABEL} {date}")
+        output_document.add_paragraph(f"{MODE_LABEL} {mode}")
+        output_document.add_paragraph(f"{SUMMARY_LABEL} {summary}")
+        output_document.add_paragraph().add_run().add_break(WD_BREAK.LINE)
+
+    output_document.save(f"{OUTPUT_FILENAME}{DOCX_EXTENSION}")
+
+    # print([x.text for x in document.paragraphs if x.text == "Data:"])
+    # for paragraph in document.paragraphs:
+    #     print(paragraph.text)
+
+
+def extract_information(input_document: Document) -> Tuple[str, str, str]:
+    pass
 
 
 def prepare_args() -> Namespace:
